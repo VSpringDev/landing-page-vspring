ca8b7c166affba8885c23a5f257de98cc8092b1d
Ayub-Budi <ayubbudisantoso09@gmail.com>
2025-06-15 20:21:26 +0700

    fix(global.css) : perbaikan import font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
