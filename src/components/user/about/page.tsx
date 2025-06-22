ea54a040c04f85367781b50041621fd0dd21dc30
anggarchmdi <anggarchmdi@gmail.com>
2025-06-22 22:56:45 +0700

    ganti L boss

--- src/components/user/about/page.tsx
+++ src/components/user/about/page.tsx
@@ -11,7 +11,7 @@ export default function About() {
 
       <div className="w-[90%] h-[70%]  bg-black/30 rounded-br-[100px] rounded-tl-[100px] shadow-lg flex items-center justify-center gap-3 z-10">
         <div className="flex flex-col items-center justify-center gap-4">
-          <img src="/logo.png" alt="" className="w-60 h-60" />
+          <img src="/Logo.png" alt="" className="w-60 h-60" />
         </div>
         <div className="flex-1 p-8 flex flex-col items-center justify-center max-w-[600px]">
           <h1 className="text-white text-4xl font-bold mb-4">About V-Spring Dev</h1>
