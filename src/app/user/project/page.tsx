2320b98cb45710cb4e35bc356813c53e80d5d5d1
Ayub-Budi <ayubbudisantoso09@gmail.com>
2025-07-07 20:16:51 +0700

    update

--- src/app/user/project/page.tsx
+++ src/app/user/project/page.tsx
@@ -1,5 +1,16 @@
 "use client"
 
 export default function Project() {
-
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-[url('/bg-2.jpg')] bg-cover bg-center bg-blend-overlay bg-black/70">
+      <div className="flex flex-col w-[90%] h-[500px] shadow-lg overflow-hidden backdrop-blur-3xl rounded-[50px] bg-black/35">
+        <div className="flex-1 flex flex-col gap-3 items-center justify-center">
+          <h1 className="text-transparent text-8xl font-bold bg-[url('/bg-2.jpg')] bg-cover bg-center bg-clip-text leading-[1.2]">Projects</h1>
+          <p className="text-white text-lg text-center max-w-2xl">
+            Here you can showcase your projects and achievements.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/user/services/page.tsx
+++ src/components/user/services/page.tsx
@@ -2,14 +2,50 @@
 
 function Card({ title, description }: { title: string, description: string }) {
   return <>
-    <div className="bg-gray-200 p-6 rounded-lg shadow-lg mb-6">
+    <div className="bg-gray-200 rounded-2xl">
+      <div className="bg-gray-100 w-[50px] h-[50px] rounded-br-2xl relative">
+        <div className="w-[24px] h-[24px] absolute top-[-8px] -right-[16px] bg-transparent rounded-tl-full border-t-[8px] border-l-[8px] border-gray-100"></div>
+        <div className="w-[24px] h-[24px] absolute bottom-[-16px] -left-[8px] bg-transparent rounded-tl-full border-t-[8px] border-l-[8px] border-gray-100"></div>
+      </div>
+      <div className="p-6 rounded-lg mb-6">
         <h2 className="text-2xl font-bold mb-2">{title}</h2>
         <p className="text-gray-700">{description}</p>
+      </div>
     </div>
   </>;
 }
 
 export default function Services() {
+  let data = [
+    {
+      title: "Web Development",
+      description: "Building responsive and dynamic websites."
+    },
+    {
+      title: "Mobile App Development",
+      description: "Creating user-friendly mobile applications."
+    },
+    {
+      title: "UI/UX Design",
+      description: "Designing intuitive user interfaces and experiences."
+    },
+    {
+      title: "Cloud Solutions",
+      description: "Implementing scalable cloud-based solutions."
+    },
+    {
+      title: "DevOps Services",
+      description: "Streamlining development and operations."
+    },
+    {
+      title: "IOT Solutions",
+      description: "Designing and implementing IoT solutions."
+    },
+    {
+      title: "Maintenance Services",
+      description: "Providing ongoing maintenance and support."
+    }
+  ]
   return (
     <>
       <div className="mx-auto w-full px-[50px] py-10">
@@ -18,13 +54,9 @@ export default function Services() {
         </h1>
 
         <div className="grid grid-cols-4 gap-6 p-6">
-          <Card title="Web Development" description="Building responsive and dynamic websites." />
-          <Card title="Mobile App Development" description="Creating user-friendly mobile applications." />
-          <Card title="UI/UX Design" description="Designing intuitive user interfaces and experiences." />
-          <Card title="Cloud Solutions" description="Implementing scalable cloud-based solutions." />
-          <Card title="DevOps Services" description="Streamlining development and operations." />
-          <Card title="IOT Solutions" description="Designing and implementing IoT solutions." />
-          <Card title="Maintenance Services" description="Providing ongoing maintenance and support." />
+            {data.map((item, index) => {
+              return <Card title={item.title} description={item.description} key={index} />
+            })}
         </div>
       </div>
     </>
